d9faabc83e6e5ab427bd602c201d8be4c21cf664
lefmarna <panume5ya@gmail.com>
2022-05-09 23:09:18 +0900

    feat: 各ページの内容を判別できるように実装

--- pages/code/index.tsx
+++ pages/code/index.tsx
@@ -4,7 +4,9 @@ import styles from '../../styles/Home.module.css'
 const Code: NextPage = () => {
   return (
     <div className={styles.container}>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <p>コード</p>
+      </main>
     </div>
   )
 }
--- pages/other/index.tsx
+++ pages/other/index.tsx
@@ -4,7 +4,9 @@ import styles from '../../styles/Home.module.css'
 const Other: NextPage = () => {
   return (
     <div className={styles.container}>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <p>その他</p>
+      </main>
     </div>
   )
 }
