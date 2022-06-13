1c69e1ae46e0aabb477c1626604bacfaa7a0672a
lefmarna <panume5ya@gmail.com>
2022-06-13 23:36:35 +0900

    feat: コピペ用のページを作成

--- pages/result/index.tsx
+++ pages/result/index.tsx
@@ -1,18 +1,88 @@
+import { IconButton, InputAdornment, OutlinedInput, TextField, Tooltip } from '@mui/material'
 import type { NextPage } from 'next'
 import { useRecoilValue } from 'recoil'
-import { computerState, computerTextFieldValueState, miconState } from '../../src/store/setupState'
+import { COMPUTER_TITLE, LIBRARY_TITLE, MICON_TITLE, WHERE_STOP_TITLE } from '../../constants/setup'
+import AssignmentIcon from '@mui/icons-material/Assignment'
+import {
+  arduinoState,
+  computerState,
+  computerTextFieldValueState,
+  libraryState,
+  miconState,
+} from '../../src/store/setupState'
 import styles from '../../styles/Home.module.css'
+import { useState } from 'react'
 
 const Result: NextPage = () => {
   const computerValue = useRecoilValue(computerState)
   const computerTextFieldValue = useRecoilValue(computerTextFieldValueState)
   const miconValue = useRecoilValue(miconState)
+  const libraryValue = useRecoilValue(libraryState)
+  const arduinoValue = useRecoilValue(arduinoState)
+
+  const writeToClipboard = (clipText: string): void => {
+    navigator.clipboard.writeText(clipText).catch((e) => {
+      console.error(e)
+    })
+  }
+
+  const computerResult = computerValue !== 'other' ? computerValue : computerTextFieldValue
+
+  const resultMessage = `
+    【${COMPUTER_TITLE}】
+    ${computerResult}
+    \n
+    【${MICON_TITLE}】
+    ${miconValue}
+    \n
+    【${LIBRARY_TITLE}】
+    ${libraryValue}
+    \n
+    【Arduino IDEのバージョンを教えてください】
+    ${arduinoValue}\n
+  `
+
+  const [openTip, setOpenTip] = useState(false)
+
+  const handleCloseTip = (): void => {
+    setOpenTip(false)
+  }
+
+  const handleClickButton = (): void => {
+    setOpenTip(true)
+    writeToClipboard(resultMessage)
+  }
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <p>{computerValue}</p>
-        <p>{computerTextFieldValue}</p>
-        <p>{miconValue}</p>
+        <OutlinedInput
+          type='text'
+          value={resultMessage}
+          multiline
+          fullWidth
+          readOnly
+          endAdornment={
+            <Tooltip
+              arrow
+              open={openTip}
+              onClose={handleCloseTip}
+              disableHoverListener
+              placement='top'
+              title='コピーしました'
+            >
+              <IconButton
+                onClick={handleClickButton}
+                edge='start'
+                sx={{ mx: 'auto', mb: 'auto', py: 0 }}
+                size='large'
+                disabled={resultMessage === ''}
+              >
+                <AssignmentIcon />
+              </IconButton>
+            </Tooltip>
+          }
+        />
       </main>
     </div>
   )
