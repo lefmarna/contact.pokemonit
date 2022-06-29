9d91735e3e2fab665da377ddb09a496030940a47
lefmarna <panume5ya@gmail.com>
2022-06-29 23:29:46 +0900

    feat: コード質問用のページの準備

--- src/pages/code/index.tsx
+++ src/pages/code/index.tsx
@@ -1,7 +1,34 @@
+import { Button } from '@mui/material'
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { FormActions } from '../../components/molecules/FormActions'
 
 const Code: NextPage = () => {
-  return <p>コード</p>
+  const router = useRouter()
+
+  const onClickRouterBack = () => {
+    router.back()
+  }
+
+  const onClickRouterPush = () => {
+    router.push('/code/result')
+  }
+
+  const disabled = true
+
+  return (
+    <>
+      <p> コード</p>
+      <FormActions>
+        <Button onClick={onClickRouterBack} variant='outlined' sx={{ width: 1 }} color='inherit'>
+          戻る
+        </Button>
+        <Button onClick={onClickRouterPush} variant='contained' sx={{ ml: 2, width: 1 }} disabled={disabled}>
+          次へ
+        </Button>
+      </FormActions>
+    </>
+  )
 }
 
 export default Code
