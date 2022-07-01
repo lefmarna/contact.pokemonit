e9bc7f308f7b52ea0805c79764fa98b3a3cd2ac2
lefmarna <panume5ya@gmail.com>
2022-07-01 22:59:03 +0900

    feat: コードのページの実装開始

---
+++ src/constants/code.ts
@@ -0,0 +1,1 @@
+export const CODE_TITLE = '使われているコードはどちらになりますか？'
--- src/pages/code/index.tsx
+++ src/pages/code/index.tsx
@@ -1,21 +1,38 @@
 import { Button } from '@mui/material'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import { ChangeEvent } from 'react'
+import { useRecoilState } from 'recoil'
 import { FormActions } from '../../components/molecules/FormActions'
 import RouterBackButton from '../../components/molecules/RouterBackButton'
+import TextFieldForm from '../../components/molecules/TextFieldForm'
+import { CODE_TITLE } from '../../constants/code'
+import { codeState } from '../../store/codeState'
 
 const Code: NextPage = () => {
+  // 使われているコードはどちらになりますか？
+  const [codeValue, setCodeValue] = useRecoilState(codeState)
+  const onChangeCodeValue = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setCodeValue(e.target.value)
+  }
+
   const router = useRouter()
 
   const onClickRouterPush = () => {
     router.push('/code/result')
   }
 
-  const disabled = true
+  const disabled = codeValue === ''
 
   return (
     <>
-      <p> コード</p>
+      <TextFieldForm
+        formLabel={CODE_TITLE}
+        variant='standard'
+        value={codeValue}
+        onChange={onChangeCodeValue}
+        placeholder='ポケモンBDSPの自動孵化コード'
+      />
       <FormActions>
         <RouterBackButton />
         <Button onClick={onClickRouterPush} variant='contained' sx={{ ml: 2, width: 1 }} disabled={disabled}>
